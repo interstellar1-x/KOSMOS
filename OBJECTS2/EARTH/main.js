f7f11e3971d9a2cf148d41291574b529f8d5f5a4
interstellar1-x <informatic4f@gmail.com>
2026-05-21 01:51:53 +0200

    rfg

--- OBJECTS2/EARTH/main.js
+++ OBJECTS2/EARTH/main.js
@@ -3,9 +3,11 @@ import { OrbitControls } from "jsm/controls/OrbitControls.js";
 import getStarfield from "./getStarfield.js";
 import { getFresnelMat } from "./getFresnelMat.js";
 
+const PLANETS = "https://threejs.org/examples/textures/planets/";
+
 const TEXTURES = {
   earth: "./earthmap.jpg",
-  clouds: "./earth_clouds_1024.png",
+  clouds: `${PLANETS}earth_clouds_1024.png`,
 };
 
 function loadTextureAsync(loader, url) {
